119035b433d333cffeb6379af458f79793c979b8
Jesper van den Berg <jesper.vdberg@gmail.com>
2021-09-22 21:49:20 +0200

    save temp results unit tests

---
+++ src/test/java/nl/ns/bi_automation/service/MetadataMonsterTest.java
@@ -0,0 +1,18 @@
+package nl.ns.bi_automation.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetadataMonsterTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getPipelineById() {
+    }
+}
