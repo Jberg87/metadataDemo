a3ea248b8804adb826844a867ddb36eb8f0ec7d8
Jesper van den Berg <jesper.vdberg@gmail.com>
2021-11-22 22:21:43 +0100

    Voeg een succesvolle unit test toe

--- src/test/java/nl/ns/bi_automation/service/RegressionTest.java
+++ src/test/java/nl/ns/bi_automation/service/RegressionTest.java
@@ -32,6 +32,12 @@ class RegressionTest {
     @Test
     void assertTestXmlAreSimilar() throws IOException {
         utils.assertFileExistAndEqualsReference("C:\\dev\\Git\\metadataDemo\\src\\test\\resources\\output\\",
+                "C:\\dev\\Git\\metadataDemo\\zz_results\\", "RDV mapping Persoon_H.xml");
+    }
+
+    @Test
+    void assertTestXmlAreNotSimilar() throws IOException {
+        utils.assertFileExistAndEqualsReference("C:\\dev\\Git\\metadataDemo\\src\\test\\resources\\output\\",
                 "C:\\dev\\Git\\metadataDemo\\zz_results\\", "RDV mapping Persoon_S_DD.xml");
     }
 }
